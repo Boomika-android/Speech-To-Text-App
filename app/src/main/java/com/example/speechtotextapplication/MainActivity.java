cf7a0f2d6c217883ab26ad1d3b1dd61b3a62bdae
Boomika <boomika.gajendiran@rapidcare.ai>
2025-01-06 12:38:43 +0530

    second commit

--- app/src/main/java/com/example/speechtotextapplication/MainActivity.java
+++ app/src/main/java/com/example/speechtotextapplication/MainActivity.java
@@ -3,24 +3,18 @@ package com.example.speechtotextapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView mic;
     TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,28 +27,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        /* This was the Activity which is used to show the User Interface to the users which means to loads the layout
-        to the AVD.
-         */
-
-
-        mic = findViewById(R.id.imageView);
         text = findViewById(R.id.textView);
-
-        mic.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Tap to Speak");
-
-                try {
-                    startActivityForResult(intent, 123);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 
     @Override

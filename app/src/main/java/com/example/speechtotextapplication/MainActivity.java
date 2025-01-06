f17fa175ad803cff23ce20d9a71160c6a0cfce7b
Boomika <boomika.gajendiran@rapidcare.ai>
2025-01-06 12:48:05 +0530

    fourth commit

--- app/src/main/java/com/example/speechtotextapplication/MainActivity.java
+++ app/src/main/java/com/example/speechtotextapplication/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.speechtotextapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
